d678064bad57c8c46bfc06aa0f2a40c0846f01e2
Davide <davide@fortyzone.it>
2018-04-06 16:00:18 +0200

    Single file and single directory working (for zip only)

--- Homeworks/HW1/2/2.sh
+++ Homeworks/HW1/2/2.sh
@@ -17,23 +17,41 @@ fi
 # test for permissions
 
 function handleExtracted() {
+    thisDir=`dirname $1`
+    echo Directory: $thisDir
+    if [ ! -d "$thisDir/out/" ]; then 
+        mkdir "$thisDir/out"
+        echo out path: `pwd`
+    fi
     cd _extracted
-    fileName=$1
+    zipName=`basename $1`
+    zipNameLength=`echo "$zipName" | wc -c`
     ext=$2
+    extLength=`echo "$ext" | wc -c`
     numFiles=`find . -mindepth 1 -maxdepth 1 -type f | wc -l`
     numDirs=`find . -mindepth 1 -maxdepth 1 -type d | wc -l`
 
     # Single file case
     if [[ $numFiles == 1 && $numDirs == 0 ]]; then
-        extLength=`echo "$ext" | wc -c`
-        nameLength=`echo "$fileName" | wc -c`
-        fileName=`echo "$fileName" | cut -c 1-$((nameLength-extLength))`
-        echo $fileName
-    fi
-
-    # Single directory case 
+        fileName=`ls`
+        fileName="$zipName.$fileName"
+        mv `ls` $fileName
+        mv $fileName "../$thisDir/out"
+        rm -f $zipName
+        
+    ## Single directory case
+    elif [[ $numFiles == 0 && $numDirs == 1 ]]; then
+        dirName=`ls`
+        dirName="$zipName.$dirName"
+        mv `ls` $dirName
+        cp "../$thisDir/$zipName" $dirName      # change to mv after testing
+        mv $dirName "../$thisDir/out"
 
     # Mixed content case
+    else
+        echo mixed content
+        
+    fi
 
     cd ..
     rm -rf _extracted
@@ -53,30 +71,21 @@ function extractFile() {
         unzip -q $file -d _extracted </dev/null &>/dev/null &
         wait            # resume after background unzipping is done
         handleExtracted $file ".zip"
-    #elif [[ $file == *.tar ]]; then
-    #    echo
-    ##elif [[ $file == *.tgz || $file == *.tar.gz ]]; then
-    #    echo 
-    #elif [[ $file == *.tbz || $file == *.tar.bz || $file == *.tar.bz2 ]]; then
-    #    echo 
-    #elif [[ $file == *.gz ]]; then
-    #    echo 
-    #elif [[ $file == *.bz || $file == *.bz2 ]]; then
-    #    echo 
     fi
 }
 
 function scan() {
     currDir=$1
+    subdirs=`find $currDir -mindepth 1 -maxdepth 1 -type d`
+
+    for f in `find $currDir -mindepth 1 -maxdepth 1 -type f -iregex ".*\.\(zip\|tgz\|gz\|tbz\|bz2\|bz\|tar\)$"`; do
+        extractFile $f
+    done
     
-    for d in `find $currDir -mindepth 1 -maxdepth 1 -type d`; do
+    for d in $subdirs; do
         scan $d
     done
     
-    for f in `find $currDir -mindepth 1 -maxdepth 1 -type f -regex ".*\.\(zip\|tgz\|gz\|tbz\|bz\|bz2\|tar\)$"`; do
-        # the regex should be case insensitive !!
-        extractFile $f
-    done
 }
 
 scan $rootDir
