5ab4b9e748c3c506dbb972ee23d7a9f4b1b4f058
Davide <davide@fortyzone.it>
2018-04-14 22:31:22 +0200

    getOldScore done

--- Homeworks/HW1/1/1.sh
+++ Homeworks/HW1/1/1.sh
@@ -52,31 +52,36 @@ function getScoreFromCSV() {
     if [[ $foundLine != "" ]]; then
         tempScore=`echo "$foundLine" | awk -F',' '{ print $4 }'`
         tempScore=$tempScore.`echo "$foundLine" | awk -F',' '{ print $5 }'`
-        
     fi
 }
 
 function getLatestOldScore() {
     cd $1
     
-    files=`ls | grep "res_[0-9]\{4,4\}_[0-9]\{1,2\}_[0-9]\{1,2\}_L[0-9]\{1,1\}\.csv" | tac`
-
-    l3Files=(`echo $files | grep "L3"`)
-    l2Files=(`echo $files | grep "L2"`)
-    l1Files=(`echo $files | grep "L1"`)
-
-    sessionsCount=`echo "$l3Files" | wc -l`
+    l3Files=(`find -maxdepth 1 -type f -regextype posix-extended -regex "^\./res_[0-9]{4,4}_[0-9]{1,2}_[0-9]{1,2}_L3\.csv$" | sort -r`)
+    l2Files=(`find -maxdepth 1 -type f -regextype posix-extended -regex "^\./res_[0-9]{4,4}_[0-9]{1,2}_[0-9]{1,2}_L2\.csv$" | sort -r`)
+    l1Files=(`find -maxdepth 1 -type f -regextype posix-extended -regex "^\./res_[0-9]{4,4}_[0-9]{1,2}_[0-9]{1,2}_L1\.csv$" | sort -r`)
+    
+    sessionsCount=${#l3Files[@]}
     
     for (( i=0; i<$sessionsCount; i++ )); do
         getScoreFromCSV ${l3Files[$i]}
         if [[ $tempScore != "" ]]; then
-            level3Score=$tempScore
-            echo $level3Score
+            score=$tempScore
+            getScoreFromCSV ${l2Files[$i]}
+            score=`echo $score + $tempScore | bc`
+            getScoreFromCSV ${l1Files[$i]}
+            score=`echo $score + $tempScore | bc`
             break
         fi
     done
 
+    if [[ $score > 31 ]]; then
+        $score=31
+    fi
+    echo $score
+
     cd ..
 }
 
-getLatestOldScore "input_output.1/inp.1/old_res23403"
+getLatestOldScore "data/inp.1/old_res23403"
